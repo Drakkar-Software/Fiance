d98e9421c3a7d4b28d10149068b2dc107303fc64
Herklos <herklos@drakkar.software>
2026-04-05 20:47:02 +0200

    Add web CI
    Signed-off-by: Herklos <herklos@drakkar.software>

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,17 @@
+const path = require('node:path')
+const { withNativeWind } = require('nativewind/metro')
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname
+const workspaceRoot = projectRoot
+const config = getDefaultConfig(projectRoot)
 
-config.resolver.unstable_conditionNames = [
-  "react-native",
-  "browser",
-  "require",
-];
+config.resolver.unstable_enableSymlinks = true
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+]
 
-config.resolver.assetExts = [...(config.resolver.assetExts || []), "wasm"];
+if (!config.resolver.assetExts.includes('wasm')) config.resolver.assetExts.push('wasm')
 
 module.exports = withNativeWind(config, { input: "./global.css" });
